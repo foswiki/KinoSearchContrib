a3915e5f38c51acdd6a99b72bf2278c1834bc858
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-11 23:53:42 +0000

    Item8581: history update and republish
    git-svn-id: http://svn.foswiki.org/trunk/KinoSearchContrib@6726 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/KinoSearchContrib.pm
+++ lib/Foswiki/Contrib/KinoSearchContrib.pm
@@ -20,6 +20,6 @@ use strict;
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 our $VERSION = '$Rev: 4913 $';
-our $RELEASE = '1.22';
-our $SHORTDESCRIPTION = 'Fast indexed search with indexing of attachments like Word, Excel and PDF';
+our $RELEASE = '1.23';
+our $SHORTDESCRIPTION = 'Fast indexed search including attachments like Word, Excel, and PDF';
 
